5834b23f4bc0d8f53e9b5e403904e88be20dd70e
SyndicateCorp <riyatabdul11@gmail.com>
2023-10-28 09:32:17 +0700

    UTS
    UTS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'UTS212102435';
+  nama = 'Andi Riyadl Abdul Aziz Setiaraja';
+  nim = '212102435';
 }
